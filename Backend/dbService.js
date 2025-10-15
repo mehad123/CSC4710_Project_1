8fbecf36d503e33d2632ae94ad5582a71e2e9a96
saleh-programs <muradsaleh2022@gmail.com>
2025-10-15 13:46:58 -0400

    Added docker configs. Just install docker and call docker compose up (with --build when code changes). You can still work locally, this won't interfere with anything.

--- Backend/dbService.js
+++ Backend/dbService.js
@@ -5,48 +5,39 @@ const dotenv = require('dotenv');
 dotenv.config(); // read from .env file
 
 let instance = null; 
+let connection = null;
 
-
-// if you use .env to configure
-console.log("HOST: " + process.env.HOST);
-console.log("DB USER: " + process.env.DB_USER);
-console.log("PASSWORD: " + process.env.PASSWORD);
-console.log("DATABASE: " + process.env.DATABASE);
-console.log("DB PORT: " + process.env.DB_PORT);
-
-const connection = mysql.createConnection({
-     host: process.env.HOST,
-     user: process.env.DB_USER,        
-     password: process.env.PASSWORD,
-     database: process.env.DATABASE,
-     port: process.env.DB_PORT
-});
-
-
-// if you configure directly in this file, there is a security issue, but it will work
-/*
-const connection = mysql.createConnection({
-     host:"localhost",
-     user:"root",        
-     password:"",
-     database:"web_app",
-     port:3306
-});
-*/
-
-
-connection.connect((err) => {
-     if(err){
-        console.log(err.message);
-     }
-     console.log('db ' + connection.state);    // to see if the DB is connected or not
-});
+let reconnectTimer = setInterval(() => {
+   connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,        
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT
+   });
+   connection.connect((err) => {
+      if(err){
+         console.log(err.message);
+      }
+      if (connection.state === "connected"){
+         connection.query(`
+            CREATE TABLE IF NOT EXISTS names (
+               id INT AUTO_INCREMENT PRIMARY KEY,
+               name VARCHAR(200),
+               date_added DATE
+            );
+         `);
+         clearInterval(reconnectTimer)
+      }
+      console.log('db ' + connection.state);  
+   });
+}, 2000);
 
 // the following are database functions, 
 
 class DbService{
     static getDbServiceInstance(){ // only one instance is sufficient
-        return instance? instance: new DbService();
+        return instance ? instance : instance = new DbService();
     }
 
    /*
