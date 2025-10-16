37c421bf549f5f84adbb887efc745cec09183c7b
saleh-programs <muradsaleh2022@gmail.com>
2025-10-15 23:51:41 -0400

    reduced sample code clutter in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,3 @@
-// Backend: application services, accessible by URIs
-
-
 const express = require('express')
 const cors = require ('cors')
 const dotenv = require('dotenv')
@@ -15,131 +12,65 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 
-// create
-app.post('/insert', (request, response) => {
-    console.log("app: insert a row.");
-    // console.log(request.body); 
-
+app.post('/insert', async (request, response) => {
     const {name} = request.body;
-    const db = dbService.getDbServiceInstance();
 
-    const result = db.insertNewName(name);
- 
-    // note that result is a promise
-    result 
-    .then(data => response.json({data: data})) // return the newly added row to frontend, which will show it
-   // .then(data => console.log({data: data})) // debug first before return by response
-   .catch(err => console.log(err));
+    const db = dbService.getDbServiceInstance();
+    const result = await db.insertNewName(name)
+    .catch(err=>console.error(err));
+    response.json({data: result});
 });
 
 
 
-
-// read 
-app.get('/getAll', (request, response) => {
-    
+app.get('/getAll', async (request, response) => {
     const db = dbService.getDbServiceInstance();
 
-    
-    const result =  db.getAllData(); // call a DB function
-
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
+    const result = await db.getAllData()
+    .catch(err=>console.error(err));
+    response.json({data: result})
 });
 
 
-app.get('/search/:name', (request, response) => { // we can debug by URL
-    
+app.get('/search/:name', async (request, response) => { 
     const {name} = request.params;
     
-    console.log(name);
-
     const db = dbService.getDbServiceInstance();
 
     let result;
-    if(name === "all") // in case we want to search all
-       result = db.getAllData()
+    if(name === "all") 
+       result = await db.getAllData()
+        .catch(err=>console.error(err));
     else 
-       result =  db.searchByName(name); // call a DB function
-
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
+       result = await db.searchByName(name)
+        .catch(err=>console.error(err)); 
+    response.json({data: result});
 });
 
 
-// update
-app.patch('/update', 
-     (request, response) => {
-          console.log("app: update is called");
-          //console.log(request.body);
-          const{id, name} = request.body;
-          console.log(id);
-          console.log(name);
-          const db = dbService.getDbServiceInstance();
+app.patch('/update', async (request, response) => {
+    const{id, name} = request.body;
 
-          const result = db.updateNameById(id, name);
-
-          result.then(data => response.json({success: true}))
-          .catch(err => console.log(err)); 
-
-     }
-);
-
-// delete service
-app.delete('/delete/:id', 
-     (request, response) => {     
-        const {id} = request.params;
-        console.log("delete");
-        console.log(id);
-        const db = dbService.getDbServiceInstance();
-
-        const result = db.deleteRowById(id);
-
-        result.then(data => response.json({success: true}))
-        .catch(err => console.log(err));
-     }
-)   
-
-// debug function, will be deleted later
-app.post('/debug', (request, response) => {
-    // console.log(request.body); 
-
-    const {debug} = request.body;
-    console.log(debug);
-
-    return response.json({success: true});
-});   
-
-// debug function: use http://localhost:5050/testdb to try a DB function
-// should be deleted finally
-app.get('/testdb', (request, response) => {
-    
     const db = dbService.getDbServiceInstance();
 
-    
-    const result =  db.deleteById("14"); // call a DB function here, change it to the one you want
-
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
+    const result = await db.updateNameById(id, name)
+    .catch(err=>console.error(err));
+    response.json({data: result})
 });
 
+app.delete('/delete/:id', async (request, response) => {     
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
 
-// set up the web server listener
-// if we use .env to configure
-/*
+    await db.deleteRowById(id)
+    .catch(err=>console.error(err));
+
+    response.json({success: true});
+})   
+
+
 app.listen(process.env.PORT, 
     () => {
-        console.log("I am listening on the configured port " + process.env.PORT)
-    }
-);
-*/
-
-// if we configure here directly
-app.listen(5050, 
-    () => {
-        console.log("I am listening on the fixed port 5050.")
+        console.log(`I am listening on port ${process.env.PORT}.`)
     }
 );
